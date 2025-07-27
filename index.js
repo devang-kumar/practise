1fcb9c1a61d3b6a3c110313dea5cc6095c12692e
DEVANG KUMAR <devangkumar12112005@gmail.com>
2025-07-27 05:51:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,49 @@
-let http = require("http");
-let fs = require("fs");
-let url = require("url");
-let qs = require("querystring");
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const qs = require("querystring");
 
-const PORT = process.env.PORT || 3000;
 let todos = [];
 
-http.createServer((req, res) => {
-    let parsedUrl = url.parse(req.url, true);
+const PORT = process.env.PORT || 3000;
 
+http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
     if (req.method === "GET" && parsedUrl.pathname === "/") {
         fs.readFile("index.html", (err, data) => {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(data);
         });
     }
-
     else if (req.method === "GET" && parsedUrl.pathname === "/todos") {
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify(todos));
     }
-
     else if (req.method === "POST" && parsedUrl.pathname === "/add") {
         let body = "";
-        req.on("data", chunk => {
-            body += chunk.toString();
-        });
+        req.on("data", chunk => body += chunk.toString());
         req.on("end", () => {
-            const { todo } = qs.parse(body);
-            if (todo) todos.push(todo);
-            res.writeHead(302, { Location: "/" });
+            const formData = qs.parse(body);
+            if (formData.todo) {
+                todos.push(formData.todo);
+            }
+            res.writeHead(302, { "Location": "/" });
             res.end();
         });
     }
-
     else if (req.method === "GET" && parsedUrl.pathname === "/delete") {
         const index = parseInt(parsedUrl.query.index);
-        if (!isNaN(index) && index >= 0 && index < todos.length) {
+        if (!isNaN(index)) {
             todos.splice(index, 1);
         }
-        res.writeHead(302, { Location: "/" });
+        res.writeHead(302, { "Location": "/" });
         res.end();
     }
-
     else {
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("404 Not Found");
     }
 
 }).listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`✅ Server running at http://localhost:${PORT}`);
 });
